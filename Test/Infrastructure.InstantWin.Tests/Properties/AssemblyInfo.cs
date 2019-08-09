6c631c5d0df2cc4d28629504fc5c8db97417b538
Irina Nalijaona <nalijaona.andriamifidy@proximitybbdo.fr>
2019-08-10 00:27:30 +0400

    Added Test for Captcha and InstantWin

---
+++ Test/Infrastructure.InstantWin.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Infrastructure.InstantWin.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Infrastructure.InstantWin.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b5c5ced-1cf5-4e3c-b63b-eb981f7056a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
